040d8d4b4744607eb00d5e0b975c4205032c86dc
Christopher Bülte <buelte@math.lmu.de>
2024-05-15 15:28:24 +0200

    Update

--- losses.py
+++ losses.py
@@ -156,5 +156,134 @@ class LpLoss_energy(object):
 
     def __call__(self, y_pred, y, **kwargs):
         return self.es(y_pred, y)
+    
 
+
+def lp_norm(x, y, const, p=2):
+    """Calculates the Lp norm between two tensors
+
+    Args:
+        x (Tensor): First tensor of shape (B, n_samples_x, flatted_dims).
+        y (Tensor): Second tensor of shape (B, n_samples_y, flatted_dims).
+        const (int, optional): Grid spacing.
+        p (int, optional): Order of the norm. Defaults to 2.
+
+    Returns:
+        Tensor: Lp norm
+    """
+    norm = const*torch.cdist(x, y, p = p)
+    return norm
+
+
+def complex_norm(x, y, **kwargs):
+    """Calculates the complex norm between two tensors
+
+    Args:
+        x (Tensor): First tensor of shape (B, n_samples_x, flatted_dims).
+        y (Tensor): Second tensor of shape (B, n_samples_y, flatted_dims).
+
+    Returns:
+        Tensor: Complex norm norm
+    """
+    # Calculate dot product
+    x_expand = torch.unsqueeze(x, dim=2)
+    y_expand = torch.unsqueeze(y, dim=1)
+    diff = x_expand - y_expand
+    norm = torch.sqrt(torch.sum(diff * torch.conj(diff), dim = -1))
+    
+    return torch.real(norm)
+
+
+class EnergyScore(object):
+    def __init__(self, d = 1, type = "p", reduction = 'mean', reduce_dims = True, **kwargs):
+        super().__init__()
+
+        self.d = d
+        self.type = type
+        self.reduction = reduction
+        self.reduce_dims = reduce_dims
+        self.p = kwargs.get('p', 2)
+        self.L = kwargs.get('L', 2*math.pi)
+
+        if isinstance(self.L, float):
+            self.L = [self.L]*self.d
+
+        if self.type == "complex":
+            self.norm = complex_norm
+        else:
+            self.norm = lp_norm
+
+    def reduce(self, x):
+        if self.reduction == "sum":
+            x = torch.sum(x, dim=0, keepdim=True)
+        else:
+            x = torch.mean(x, dim=0, keepdim=True)
+        return x
+    
+    def uniform_h(self, x):
+        h = [0.0] * self.d
+        for j in range(self.d, 0, -1):
+            h[-j] = self.L[-j] / x.size(-j-1)
+        return h
+
+    def calculate_score(self, x, y, h = None):
+        """Calculates the energy score for different metrics
+
+        Args:
+            x (_type_): Model prediction (Batch size, ..., n_samples)
+            y (_type_): Target (Batch size, ..., 1)
+            h (_type_, optional): _description_. Defaults to None.
+
+        Returns:
+            _type_: Energy score
+        """
+        n_samples = x.size()[-1]
+
+        # Add additional dimension if necessary
+        if len(x.size()) != len(y.size()):
+            y = torch.unsqueeze(y, dim=-1)
+
+
+        # Restructure tensors to shape (Batch size, n_samples, flatted dims)
+        x_flat = torch.swapaxes(torch.flatten(x, start_dim=1, end_dim=-2), 1, 2)
+        y_flat = torch.swapaxes(torch.flatten(y, start_dim=1, end_dim=-2), 1, 2)
+
+        #Assume uniform mesh
+        if h is None:
+            h = self.uniform_h(x)
+        else:
+            if isinstance(h, float):
+                h = [h]*self.d
+        if self.type == "lp":
+            const = math.prod(h)**(1.0/self.p)
+        else:
+            const = 1.0
+
+        # Calculate energy score
+        term_1 = torch.mean(self.norm(x_flat, y_flat, const = const, p = self.p), dim=(1,2))
+        term_2 = torch.sum(self.norm(x_flat, x_flat, const = const, p = self.p), dim=(1,2))
+        score = term_1 - term_2 / (2 * n_samples * (n_samples - 1))
+
+        # Reduce
+        return self.reduce(score).squeeze() if self.reduce_dims else score
+
+    def __call__(self, y_pred, y, **kwargs):
+        return self.calculate_score(y_pred, y, **kwargs)
+    
+    #test with main method
+if __name__ == '__main__':
+    # set torch seed
+    torch.manual_seed(0)
+    input = torch.rand(10, 10, 5)
+    truth = torch.ones(10, 10, 1)
+    es = EnergyScore(d = 1, p = 4, type = "lp")
+    lp_es = LpLoss_energy(d = 1, p = 4)
+
+    score = es(input, truth)
+    print(score.shape)
+    print(score)
+
+    score2 = lp_es(input, truth)
+    print(score2.shape)
+    print(score2)
   
