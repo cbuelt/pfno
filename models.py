f387e1db3a1c602d9abafead0f2ffc2edb663ae0
Christopher Bülte <buelte@math.lmu.de>
2024-05-23 15:29:30 +0200

    Update

--- models.py
+++ models.py
@@ -13,6 +13,8 @@ from neuralop.layers.skip_connections import skip_connection
 from neuralop.models.base_model import BaseModel
 from neuralop.models.fno import FNO, partialclass
 from neuralop.models.uno import UNO
+from layers import SpectralConv_complex, MLP_complex
+from complexPyTorch.complexFunctions import complex_relu
 
 
 
@@ -399,14 +401,173 @@ class UNO_reparam(nn.Module):
         return x
 
 
+
+
+
+class FNO_complex(FNO, name = "FNO_copmlex"):
+    def __init__(
+    self,
+    n_modes,
+    hidden_channels,
+    n_samples,
+    in_channels=3,
+    out_channels=1,
+    lifting_channels=256,
+    projection_channels=256,
+    n_layers=4,
+    output_scaling_factor=None,
+    max_n_modes=None,
+    fno_block_precision="full",
+    use_mlp=False,
+    mlp_dropout=0,
+    mlp_expansion=0.5,
+    non_linearity=F.gelu,
+    stabilizer=None,
+    norm=None,
+    preactivation=False,
+    fno_skip="linear",
+    mlp_skip="soft-gating",
+    separable=False,
+    factorization=None,
+    rank=1.0,
+    joint_factorization=False,
+    fixed_rank_modes=False,
+    implementation="factorized",
+    decomposition_kwargs=dict(),
+    domain_padding=None,
+    domain_padding_mode="one-sided",
+    fft_norm="forward",
+    SpectralConv=SpectralConv,
+    **kwargs
+    ):
+        super().__init__(
+            n_modes=n_modes,
+            hidden_channels=hidden_channels,
+            in_channels=in_channels,
+            out_channels=out_channels,
+            lifting_channels=lifting_channels,
+            projection_channels=projection_channels,
+            n_layers=n_layers,
+            output_scaling_factor=output_scaling_factor,
+            non_linearity=non_linearity,
+            stabilizer=stabilizer,
+            use_mlp=use_mlp,
+            mlp_dropout=mlp_dropout,
+            mlp_expansion=mlp_expansion,
+            max_n_modes=max_n_modes,
+            fno_block_precision=fno_block_precision,
+            norm=norm,
+            fno_skip=fno_skip,
+            mlp_skip=mlp_skip,
+            separable=separable,
+            preactivation=preactivation,
+            factorization=factorization,
+            rank=rank,
+            joint_factorization=joint_factorization,
+            fixed_rank_modes=fixed_rank_modes,
+            implementation=implementation,
+            decomposition_kwargs=decomposition_kwargs,
+            domain_padding=domain_padding,
+            domain_padding_mode=domain_padding_mode,
+            fft_norm=fft_norm,
+            SpectralConv=SpectralConv,
+        )
+        self.n_samples = n_samples
+
+        # Add complex convolution layer
+        self.final_conv = SpectralConv_complex(
+            self.hidden_channels,
+            self.hidden_channels,
+            self.n_modes,
+            output_scaling_factor=output_scaling_factor,
+            max_n_modes=max_n_modes,
+            rank=rank,
+            fixed_rank_modes=fixed_rank_modes,
+            implementation=implementation,
+            separable=separable,
+            factorization=factorization,
+            decomposition_kwargs=decomposition_kwargs,
+            joint_factorization=joint_factorization,
+            n_layers=n_layers,
+        )
+
+        # Add a reparameterization layer
+        self.mu = MLP_complex(
+        in_channels=self.hidden_channels,
+        out_channels=out_channels,
+        hidden_channels=self.projection_channels,
+        n_layers=2,
+    )
+        
+        self.sigma = MLP_complex(
+        in_channels=self.hidden_channels,
+        out_channels=out_channels,
+        hidden_channels=self.projection_channels,
+        n_layers=2,
+    )
+        
+    def forward(self, x, n_samples = None, output_shape = None, **kwargs):
+        """TFNO's forward pass
+
+        Parameters
+        ----------
+        x : tensor
+            input tensor
+        output_shape : {tuple, tuple list, None}, default is None
+            Gives the option of specifying the exact output shape for odd shaped inputs.
+            * If None, don't specify an output shape
+            * If tuple, specifies the output-shape of the **last** FNO Block
+            * If tuple list, specifies the exact output-shape of each FNO Block
+        """
+
+        batchsize, channels, *mode_sizes = x.shape
+        order = len(self.n_modes)
+        fft_dims = list(range(-order-1, -1))
+
+        if output_shape is None:
+            output_shape = [None]*self.n_layers
+        elif isinstance(output_shape, tuple):
+            output_shape = [None]*(self.n_layers - 1) + [output_shape]
+
+        if n_samples is None:
+            n_samples = self.n_samples
+
+        x = self.lifting(x)
+
+        if self.domain_padding is not None:
+            x = self.domain_padding.pad(x)
+
+        for layer_idx in range(self.n_layers):
+            x = self.fno_blocks(x, layer_idx, output_shape=output_shape[layer_idx])
+
+        if self.domain_padding is not None:
+            x = self.domain_padding.unpad(x)
+
+        # Final layer
+        x = self.final_conv(x)
+
+        # Reprameterization layer
+        mu = self.mu(x).unsqueeze(-1)
+        sigma = complex_relu(self.sigma(x)).unsqueeze(-1)
+
+        # Reparameterization trick
+        x = mu + sigma * torch.randn(*mu.shape[:-1], n_samples, dtype = torch.complex64).to(x.device)
+
+        # Return to physical space for sanity check
+        x = torch.fft.irfftn(x, s=mode_sizes, dim=fft_dims, norm=self.fft_norm)
+
+        return x
+
+
+
 # Main method
 if __name__ == '__main__':
     # Create a model
-    model = FNO_reparam(n_modes=(64,), hidden_channels=64, in_channels=2, out_channels=1, n_samples = 10)
-    model = UNO_reparam(in_channels = 2, out_channels = 1, hidden_channels = 64, n_samples = 10, uno_out_channels=[32,64,64,32],
-                uno_n_modes=[[5,5],[5,5],[5,5],[5,5]], uno_scalings=[[1.0,1.0],[0.5,0.5],[1,1],[2,2]], n_layers = 4)
+    model = FNO_complex(n_modes=(64,64), hidden_channels=64, in_channels=2, out_channels=1, n_samples = 10)
     x = torch.randn(5, 2, 128, 128)
 
     out = model(x)
     print(out.shape)
+    print(out.dtype)
+
 
