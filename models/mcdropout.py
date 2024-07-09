39319891fa15fd6c9405c4cfc0aa7e20afbec77b
Christopher Buelte <buelte@math.lmu.de>
2024-07-09 18:22:32 +0200

    Darcy Flow Example

--- models/mcdropout.py
+++ models/mcdropout.py
@@ -1,7 +1,7 @@
 import torch
 
 def generate_mcd_samples(model, x, shape, n_samples=100):
-    samples = torch.zeros(*shape, n_samples)
+    samples = torch.zeros(*shape, n_samples).to(x.device)
     for i in range(n_samples):
         with torch.no_grad():
             samples[...,i] = model(x).detach()
