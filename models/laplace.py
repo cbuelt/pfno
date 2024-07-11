f6c8890df43d7e7cd8f4234a932536acc92ce00f
Christopher Buelte <buelte@math.lmu.de>
2024-07-11 09:39:38 +0200

    Update

--- models/laplace.py
+++ models/laplace.py
@@ -2,29 +2,44 @@ from torch.nn.utils import vector_to_parameters
 from laplace import Laplace
 import torch
 
-class LA_Wrapper():
-    def __init__(self, model: torch.nn.Module, n_samples: int = 100, 
-                 method = "last_layer", hessian_structure = "full"):
+
+class LA_Wrapper:
+    def __init__(
+        self,
+        model: torch.nn.Module,
+        n_samples: int = 100,
+        method="last_layer",
+        hessian_structure="full",
+        optimize=True,
+    ):
 
         super().__init__()
         self.model = model
         self.n_samples = n_samples
         self.method = method
         self.hessian_structure = hessian_structure
+        self.optimize = optimize
         self.model.eval()
 
-    def fit(self,train_loader):
-        self.la = Laplace(self.model, "regression",
-             subset_of_weights=self.method,
-             hessian_structure=self.hessian_structure)
-        self.la.fit(train_loader)
 
-    def optimize(self):
-        raise NotImplementedError    
-    
+    def fit(self, train_loader):
+        self.la = Laplace(
+            self.model,
+            "regression",
+            subset_of_weights=self.method,
+            hessian_structure=self.hessian_structure,
+        )
+        self.la.fit(train_loader)
+        if self.optimize:
+            self.optimize_precision()
+
+    def optimize_precision(self):
+        # Optimizer 1
+        self.la.optimize_prior_precision(pred_type="nn", method="marglik")
+
     def parameter_samples(self):
         return self.la.sample()
-        
+
     def predictive_samples(self, x):
         prediction = list()
         feats = None
@@ -37,15 +52,14 @@ class LA_Wrapper():
                     x.to(self.la._device),
                 )
                 # f-shape: (B, C, d1, ..., dn)
-                          
 
             else:
                 # Used the cached features for the rest iterations
                 f = self.la.model.last_layer(feats)
-                #f-shape: (B, d1, ..., dn, C)
+                # f-shape: (B, d1, ..., dn, C)
                 f = torch.movedim(f, -1, 1)
             prediction.append(f.detach() if not self.la.enable_backprop else f)
 
         vector_to_parameters(self.la.mean, self.la.model.last_layer.parameters())
-        prediction = torch.stack(prediction, dim = -1)
+        prediction = torch.stack(prediction, dim=-1)
         return prediction
