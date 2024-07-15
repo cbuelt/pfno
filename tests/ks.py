9ea089efa09f95628c9c9257f6c5c2aaeb156513
Christopher Bülte <buelte@math.lmu.de>
2024-07-15 15:48:07 +0200

    Initial commit

--- tests/ks.py
+++ tests/ks.py
@@ -6,7 +6,7 @@ import numpy as np
 def get_sim(grid, n_steps, dim, mesh = None, u = None, t_range = 150, dt = 0.001):
     if dim == 1:
         eq = pde.PDE({"u": "-u * d_dx(u) / 2 - laplace(u + laplace(u))"})
-        state = pde.ScalarField.random_normal(grid, mean = 0)  # generate initial condition
+        state = pde.ScalarField.random_uniform(grid, vmin = -1, vmax = 1)  # generate initial condition
     else:
       #  X,Y = mesh        
       #  data = np.sin(np.pi/25*(X+Y)+u[0])+np.sin(np.pi/25*X+u[1])+np.sin(np.pi/25*Y+u[2])
