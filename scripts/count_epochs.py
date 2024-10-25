02fe095d8be1e04201b53e2b75ac72ba32f7521b
Philipp Scholl <scholl@math.lmu.de>
2024-10-25 11:30:49 +0200

    evaluations + darcy flow uno results

--- scripts/count_epochs.py
+++ scripts/count_epochs.py
@@ -6,11 +6,17 @@ def get_log_filenames_directory(directory):
         for name in files:
             filenames.append(os.path.join(path, name))
     
-    filenames = [filename for filename in filenames if 'laplace' in filename]
-    filenames = [filename for filename in filenames if filename.endswith('.pt')]
-    filenames = [filename for filename in filenames if (not filename[:-3].endswith('la_state'))]
+    filenames = [filename for filename in filenames if not ('fno_10' in filename)]  # Ignore fno_10 folder, since they were just for evaluation
+    filenames = [filename for filename in filenames if filename.endswith('.log')]
     return filenames
 
+def get_number_training_runs(log):
+    start = 'INFO:root:Training starts now.\n'
+    return len([line for line in log if line==start])
+
+def get_number_epochs(log):
+    string = 'INFO:root:['
+    return len([line for line in log if line.startswith(string)])
 
 def get_ini_file(filename):
     directory = os.path.dirname(filename)
@@ -20,6 +26,12 @@ def get_ini_file(filename):
 if __name__=='__main__':
     filenames = get_log_filenames_directory('results/optimal_hp_multiple_seeds')
     for filename in filenames:
-        with open(filename, 'br') as file:
-            log = file.readlines()
+        print(filename)
+        if filename == 'results/optimal_hp_multiple_seeds/ks/uno/20240919_134024_ks_uno_dropout/experiment.log':
+            print('This data is corrupted (the log file seems to have broken down.)')   
         
+        with open(filename, 'r') as file:
+            log = file.readlines()
+        number_training_runs = get_number_training_runs(log)
+        number_epochs = get_number_epochs(log)
+        print(f'Average number of epochs: {number_epochs / number_training_runs:.2f}')
